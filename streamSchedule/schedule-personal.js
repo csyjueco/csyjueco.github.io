9aee37de6aeeba76795d35fe3a08fd4d61e94874
csyjueco <35493421+csyjueco@users.noreply.github.com>
2021-12-28 16:53:57 -0500

    Adding personal defaults

---
+++ streamSchedule/schedule-personal.js
@@ -0,0 +1,16 @@
+var oldschedule = [
+[`Aoi Tokimori`, new Date(`Mon Dec 27 2021 20:00:00 GMT-0500 (Eastern Standard Time)`), `REVENGING Skill 10 Sound Voltex`, ``],
+[`Aoi Tokimori`, new Date(`Wed Dec 29 2021 21:00:00 GMT-0500 (Eastern Standard Time)`), `Gen 1 Minecraft collab`, ``],
+[`Aoi Tokimori`, new Date(`Sat Jan 01 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `New Year's Karaoke!`, ``],
+[`Aoi Tokimori`, new Date(`Sun Jan 02 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `New Year's Nuggie Review Zatsudan`, ``],
+[`Fujikura Uruka`, new Date(`Mon Dec 27 2021 19:00:00 GMT-0500 (Eastern Standard Time)`), `Kuukiyomi`, ``],
+[`Fujikura Uruka`, new Date(`Tue Dec 28 2021 12:00:00 GMT-0500 (Eastern Standard Time)`), `Xmas PC Minecraft Collab`, `Exploring the PC Xmas town`],
+[`Fujikura Uruka`, new Date(`Thu Dec 30 2021 23:00:00 GMT-0500 (Eastern Standard Time)`), `Super Animal Royale`, `(try to) GET 5 TOP 5s >:)`],
+[`Fujikura Uruka`, new Date(`Sat Jan 01 2022 23:30:00 GMT-0500 (Eastern Standard Time)`), `Unarchived Guitar + Singing`, ``],
+[`Fujikura Uruka`, new Date(`Sun Jan 02 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Spiritfarer`, ``],
+[`Sara Nagare`, new Date(`Tue Dec 28 2021 00:00:00 GMT-0500 (Eastern Standard Time)`), `Crash Bandicoot N-sane Trilogy Pt.2`, ``],
+[`Sara Nagare`, new Date(`Wed Dec 29 2021 00:00:00 GMT-0500 (Eastern Standard Time)`), `To the Moon`, ``],
+[`Sara Nagare`, new Date(`Wed Dec 29 2021 20:00:00 GMT-0500 (Eastern Standard Time)`), `New Game (TBD)`, ``],
+[`Sara Nagare`, new Date(`Thu Dec 30 2021 20:00:00 GMT-0500 (Eastern Standard Time)`), `My Immortal Pt. 2`, ``],
+[`Sara Nagare`, new Date(`Sat Jan 01 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `New Year's Resolutions chat`, ``]
+];
---
+++ streamSchedule/streamers-personal.js
@@ -0,0 +1,5 @@
+var streamerSpecificStuff = [
+[`Aoi Tokimori`, `#2b5fd9`, `https://www.youtube.com/channel/UCRWF6QSuklmwY3UJHyVTQ1w`],
+[`Fujikura Uruka`, `#2fc2ca`, `https://www.youtube.com/c/FujikuraUruka`],
+[`Sara Nagare`, `#d9e96b`, `https://www.youtube.com/channel/UCkJ64W0J7R0zCSAsZfRIhVQ`]
+];
