aa8f8159e7552a332e808916aa4ef34576aa80a9
csyjueco <35493421+csyjueco@users.noreply.github.com>
2021-12-28 16:25:24 -0500

    Add files via upload

---
+++ streamSchedule/streamers-NIJI-EN.js
@@ -0,0 +1,17 @@
+var streamerSpecificStuff = [
+[`Elira Pendora`, `#95c8d8`, `https://www.youtube.com/channel/UCIeSUTOTkF9Hs7q3SGcO-Ow`],
+[`Enna Alouette`, `#858ed1`, `https://www.youtube.com/channel/UCR6qhsLpn62WVxCBK1dkLow`],
+[`Finana Ryugu`, `#79cfb8`, `https://www.youtube.com/c/FinanaRyugu-NIJISANJIEN`],
+[`Ike Eveland`, `#348ec7`, `https://www.youtube.com/channel/UC4yNIKGvy-YUrwYupVdLDXA`],
+[`Luca Kaneshiro`, `#d4af37`, `https://www.youtube.com/channel/UC7Gb7Uawe20QyFibhLl1lzA`],
+[`Millie Parfait`, `#febc87`, `https://www.youtube.com/channel/UC47rNmkDcNgbOcM-2BwzJTQ`],
+[`Mysta Rias`, `#c3552b`, `https://www.youtube.com/channel/UCIM92Ok_spNKLVB5TsgwseQ`],
+[`Nina Kosaka`, `#ff0000`, `https://www.youtube.com/channel/UCkieJGn3pgJikVW8gmMXE2w`],
+[`Petra Gurin`, `#ec9e2b`, `https://www.youtube.com/channel/UCgA2jKRkqpY_8eysPUs8sjw`],
+[`Pomu Rainpuff`, `#258e70`, `https://www.youtube.com/channel/UCP4nMSTdwU1KqYWu3UH5DHQ`],
+[`Reimu Endou`, `#b90b4a`, `https://www.youtube.com/channel/UCBURM8S4LH7cRZ0Clea9RDA`],
+[`Rosemi Lovelock`, `#f0aab8`, `https://www.youtube.com/channel/UC4WvIIAo89_AzGUh1AZ6Dkg`],
+[`Selen Tatsuki`, `#7e4eac`, `https://www.youtube.com/channel/UCV1xUwfM2v2oBtT3JNvic3w`],
+[`Shu Yamino`, `#a660a7`, `https://www.youtube.com/channel/UCG0rzBZV_QMP4MtWg6IjhEA`],
+[`Vox Akuma`, `#960018`, `https://www.youtube.com/channel/UCckdfYDGrjojJM28n5SHYrA`]
+];
