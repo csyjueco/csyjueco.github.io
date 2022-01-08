d0cfb5038df30e2f07d4112c1be1cba252ab8027
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-01-08 11:12:02 -0500

    Update ytTheatre.js

--- ytTheatre/ytTheatre.js
+++ ytTheatre/ytTheatre.js
@@ -8,7 +8,7 @@ var searchParams = new URLSearchParams(new URL(window.location.href).search);
 if (searchParams.has("v")) {
 	if (searchParams.get("v") != "") {
 		document.querySelectorAll("#ytVideo")[0].src = "https://www.youtube.com/embed/" + searchParams.get("v");
-		document.querySelectorAll("#ytChat")[0].src = "https://www.youtube.com/live_chat?v=" + searchParams.get("v") + "&embed_domain=csyjueco.github.io";
+		document.querySelectorAll("#ytChat")[0].src = "https://www.youtube.com/live_chat?v=" + searchParams.get("v") + "&embed_domain=csyjueco.github.io&dark_theme=1";
 	}
 } else {
 	window.history.pushState({}, "", window.location.href + "?v=");
