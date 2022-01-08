7f3306eb43bc2c77dd6993ad3547ab9d32936ebc
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-01-08 10:11:56 -0500

    Add files via upload

---
+++ ytTheatre/ytTheatre.js
@@ -0,0 +1,11 @@
+window.onresize = function () {
+	var chatWidth = Math.max(window.innerWidth * 0.15, 350) + "px";
+	document.documentElement.style.setProperty("--ytChatWidth", chatWidth);
+}
+
+var searchParams = new URLSearchParams(new URL(window.location.href).search);
+	
+if (searchParams.has("v")) {
+	document.querySelectorAll("#ytVideo")[0].src = "https://www.youtube.com/embed/" + searchParams.get("v");
+	document.querySelectorAll("#ytChat")[0].src = "https://www.youtube.com/live_chat?v=" + searchParams.get("v") + "&embed_domain=csyjueco.github.io";
+}
