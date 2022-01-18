47649b35aab68e7a38974ca90408525fa8b03c02
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-01-17 20:59:27 -0500

    Update schedule-personal.js

--- streamSchedule/schedule-personal.js
+++ streamSchedule/schedule-personal.js
@@ -1,9 +1,15 @@
 var oldschedule = [
-[`Aoi Tokimori`, new Date(`Sun Jan 02 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `New Year's Nuggie Review Zatsudan`, ``],
-[`Fujikura Uruka`, new Date(`Sun Jan 02 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Spiritfarer`, ``],
-[`Sara Nagare`, new Date(`Mon Jan 03 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Tropico 6`, ``],
-[`Sara Nagare`, new Date(`Mon Jan 03 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `PRISM NY Event!!`, ``],
-[`Sara Nagare`, new Date(`Wed Jan 05 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Alan Wake`, ``],
-[`Sara Nagare`, new Date(`Thu Jan 06 2022 17:00:00 GMT-0500 (Eastern Standard Time)`), `Minecraft in the Mornings!`, ``],
-[`Sara Nagare`, new Date(`Sun Jan 09 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Sara's Sunday Session`, ``]
+[`Aoi Tokimori`, new Date(`Tue Jan 18 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Guitar Practice`, ``],
+[`Aoi Tokimori`, new Date(`Tue Jan 18 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `osu! Mania`, `Training Arc`],
+[`Aoi Tokimori`, new Date(`Thu Jan 20 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Song Learning`, ``],
+[`Aoi Tokimori`, new Date(`Fri Jan 21 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `Lost in Vivo`, ``],
+[`Aoi Tokimori`, new Date(`Sat Jan 22 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `Karaoke`, `Speed Challenge`],
+[`Reiny`, new Date(`Tue Jan 18 2022 13:00:00 GMT-0500 (Eastern Standard Time)`), `Who's your mommy?`, `w/ Emma!`],
+[`Reiny`, new Date(`Wed Jan 19 2022 13:30:00 GMT-0500 (Eastern Standard Time)`), `Apex`, `w/ Kai & Nemu!`],
+[`Reiny`, new Date(`Fri Jan 21 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `Something on Twitch`, `IDK YET!`],
+[`Sara Nagare`, new Date(`Sun Jan 16 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Gen 4 Collab: Pummel Party`, `(on Sumi's channel!)`],
+[`Sara Nagare`, new Date(`Mon Jan 17 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Crash Bandicoot N'Sane Trilogy`, ``],
+[`Sara Nagare`, new Date(`Wed Jan 19 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Lobotomy Corporation`, ``],
+[`Sara Nagare`, new Date(`Wed Jan 19 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Alan Wake`, ``],
+[`Sara Nagare`, new Date(`Sun Jan 23 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Sara's Sunday Sesh`, `BUDGETING EDITION`]
 ];
