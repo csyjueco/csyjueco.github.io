98d78819c9086cd1b34609df5243c4939c3fe452
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-01-17 20:59:08 -0500

    Update streamers-personal.js

--- streamSchedule/streamers-personal.js
+++ streamSchedule/streamers-personal.js
@@ -1,5 +1,6 @@
 var streamerSpecificStuff = [
 [`Aoi Tokimori`, `#2b5fd9`, `https://www.youtube.com/channel/UCRWF6QSuklmwY3UJHyVTQ1w`],
 [`Fujikura Uruka`, `#2fc2ca`, `https://www.youtube.com/c/FujikuraUruka`],
+[`Reiny`, `#f29b3c`, `https://www.youtube.com/c/dokuganP`],
 [`Sara Nagare`, `#d9e96b`, `https://www.youtube.com/channel/UCkJ64W0J7R0zCSAsZfRIhVQ`]
 ];
