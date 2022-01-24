bc4108be8ea04a7e641a22b9940cde0e06ddd590
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-01-23 21:14:39 -0500

    Update schedule-personal.js

--- streamSchedule/schedule-personal.js
+++ streamSchedule/schedule-personal.js
@@ -1,15 +1,8 @@
 var oldschedule = [
-[`Aoi Tokimori`, new Date(`Tue Jan 18 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Guitar Practice`, ``],
-[`Aoi Tokimori`, new Date(`Tue Jan 18 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `osu! Mania`, `Training Arc`],
-[`Aoi Tokimori`, new Date(`Thu Jan 20 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Song Learning`, ``],
-[`Aoi Tokimori`, new Date(`Fri Jan 21 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `Lost in Vivo`, ``],
-[`Aoi Tokimori`, new Date(`Sat Jan 22 2022 23:00:00 GMT-0500 (Eastern Standard Time)`), `Karaoke`, `Speed Challenge`],
-[`Reiny`, new Date(`Tue Jan 18 2022 13:00:00 GMT-0500 (Eastern Standard Time)`), `Who's your mommy?`, `w/ Emma!`],
-[`Reiny`, new Date(`Wed Jan 19 2022 13:30:00 GMT-0500 (Eastern Standard Time)`), `Apex`, `w/ Kai & Nemu!`],
-[`Reiny`, new Date(`Fri Jan 21 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `Something on Twitch`, `IDK YET!`],
-[`Sara Nagare`, new Date(`Sun Jan 16 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Gen 4 Collab: Pummel Party`, `(on Sumi's channel!)`],
-[`Sara Nagare`, new Date(`Mon Jan 17 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Crash Bandicoot N'Sane Trilogy`, ``],
-[`Sara Nagare`, new Date(`Wed Jan 19 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Lobotomy Corporation`, ``],
-[`Sara Nagare`, new Date(`Wed Jan 19 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Alan Wake`, ``],
-[`Sara Nagare`, new Date(`Sun Jan 23 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Sara's Sunday Sesh`, `BUDGETING EDITION`]
+[`Reiny`, new Date(`Mon Jan 24 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `Needy Streamer Overload!`, ``],
+[`Reiny`, new Date(`Wed Jan 26 2022 15:30:00 GMT-0500 (Eastern Standard Time)`), `Big Among Us Collab!`, ``],
+[`Reiny`, new Date(`Thu Jan 27 2022 15:00:00 GMT-0500 (Eastern Standard Time)`), `Biped with Reina Sun!`, ``],
+[`Reiny`, new Date(`Fri Jan 28 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `Rimworld with Lumi Kaneko!`, ``],
+[`Reiny`, new Date(`Sat Jan 29 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `ASMR Karaoke!`, ``],
+[`Reiny`, new Date(`Sun Jan 30 2022 07:00:00 GMT-0500 (Eastern Standard Time)`), `Outlast with heart rate monitor!`, ``]
 ];
