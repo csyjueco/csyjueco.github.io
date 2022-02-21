a56ef62018db080ad92764d902ee3a791e6733cb
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-02-21 07:39:23 -0500

    Update schedule-personal.js

--- streamSchedule/schedule-personal.js
+++ streamSchedule/schedule-personal.js
@@ -1,4 +1,8 @@
 var oldschedule = [
-[`Reiny`, new Date(`Mon Feb 14 2022 10:00:00 GMT-0500 (Eastern Standard Time)`), `Reiny's 12 hour Birthday Marathon`, ``],
-[`Saruei`, new Date(`Mon Feb 14 2022 18:00:00 GMT-0500 (Eastern Standard Time)`), `Valentine's Day`, `Outfit Reveal`]
+[`Sara Nagare`, new Date(`Sun Feb 20 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Buying Foreign Snacks w/ Chat`, ``],
+[`Sara Nagare`, new Date(`Wed Feb 23 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Reading Short Stories to you`, ``],
+[`Sara Nagare`, new Date(`Wed Feb 23 2022 15:00:00 GMT-0500 (Eastern Standard Time)`), `Minecraft in the Mornings!`, ``],
+[`Sara Nagare`, new Date(`Thu Feb 24 2022 21:00:00 GMT-0500 (Eastern Standard Time)`), `Dark Souls: Remastered`, ``],
+[`Sara Nagare`, new Date(`Sun Feb 27 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Crash Bandicoot N'Sane Trilogy`, `Cortex Strikes Back`],
+[`Saruei`, new Date(`Fri Feb 25 2022 15:00:00 GMT-0500 (Eastern Standard Time)`), `New Outfit Reveal`, ``]
 ];
