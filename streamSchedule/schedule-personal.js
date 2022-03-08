edac5f6fad93eb90d6d276fa8419de21c2575f0d
csyjueco <35493421+csyjueco@users.noreply.github.com>
2022-03-08 08:46:03 -0500

    Update schedule-personal.js

--- streamSchedule/schedule-personal.js
+++ streamSchedule/schedule-personal.js
@@ -1,8 +1,14 @@
 var oldschedule = [
-[`Sara Nagare`, new Date(`Sun Feb 20 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Buying Foreign Snacks w/ Chat`, ``],
-[`Sara Nagare`, new Date(`Wed Feb 23 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Reading Short Stories to you`, ``],
-[`Sara Nagare`, new Date(`Wed Feb 23 2022 15:00:00 GMT-0500 (Eastern Standard Time)`), `Minecraft in the Mornings!`, ``],
-[`Sara Nagare`, new Date(`Thu Feb 24 2022 21:00:00 GMT-0500 (Eastern Standard Time)`), `Dark Souls: Remastered`, ``],
-[`Sara Nagare`, new Date(`Sun Feb 27 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Crash Bandicoot N'Sane Trilogy`, `Cortex Strikes Back`],
-[`Saruei`, new Date(`Fri Feb 25 2022 15:00:00 GMT-0500 (Eastern Standard Time)`), `New Outfit Reveal`, ``]
+[`Aoi Tokimori`, new Date(`Mon Mar 07 2022 21:00:00 GMT-0500 (Eastern Standard Time)`), `Escape Simulator`, `w/ Tonya`],
+[`Aoi Tokimori`, new Date(`Wed Mar 09 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Agar.io`, ``],
+[`Aoi Tokimori`, new Date(`Fri Mar 11 2022 21:00:00 GMT-0500 (Eastern Standard Time)`), `Aoi's playlist`, `with your song suggestions!`],
+[`Aoi Tokimori`, new Date(`Sun Mar 13 2022 09:00:00 GMT-0400 (Eastern Daylight Saving Time)`), `Soft songs only`, `Unarchived Karaoke`],
+[`Fujikura Uruka`, new Date(`Mon Mar 07 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Powerwash Simulator`, `Chat and sing`],
+[`Fujikura Uruka`, new Date(`Tue Mar 08 2022 23:30:00 GMT-0500 (Eastern Standard Time)`), `Minecraft`, `make house`],
+[`Fujikura Uruka`, new Date(`Fri Mar 11 2022 23:30:00 GMT-0500 (Eastern Standard Time)`), `Urufugang fashion review`, ``],
+[`Fujikura Uruka`, new Date(`Sun Mar 13 2022 21:00:00 GMT-0400 (Eastern Daylight Saving Time)`), `Minecraft collab`, `w/ Iku!!!`],
+[`Sara Nagare`, new Date(`Sun Mar 06 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Sara's Sunday Sesh`, `Eating + Tier-listing snacks #1`],
+[`Sara Nagare`, new Date(`Wed Mar 09 2022 00:00:00 GMT-0500 (Eastern Standard Time)`), `Superliminal`, ``],
+[`Sara Nagare`, new Date(`Wed Mar 09 2022 16:00:00 GMT-0500 (Eastern Standard Time)`), `Minecraft in the Mornings!`, ``],
+[`Sara Nagare`, new Date(`Thu Mar 10 2022 20:00:00 GMT-0500 (Eastern Standard Time)`), `Phasmophobia Collab`, `w/ Shiki, Aries and Isa`]
 ];
